fb220d9b8c93461e04d876955006d0cf067c7cca
Frank <zhwuzhuo@gmail.com>
2023-03-15 21:31:52 +0800

    feat: 修改基础数据模型信息

--- xx-core/src/main/java/com/github/w4o/xx/core/base/dto/BaseDataDTO.java
+++ xx-core/src/main/java/com/github/w4o/xx/core/base/dto/BaseDataDTO.java
@@ -1,5 +1,6 @@
 package com.github.w4o.xx.core.base.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,16 +10,18 @@ import java.time.LocalDateTime;
  * @author Frank
  */
 @Data
-@Schema(name = "通用分页数据模型")
-public class BasePageDataDTO {
+@Schema(name = "通用数据模型")
+public class BaseDataDTO {
 
-    @Schema(description = "创建人ID")
+    @JsonIgnore
+    @Schema(description = "创建人ID", hidden = true)
     private Long createBy;
     @Schema(description = "创建人")
     private String createUser;
     @Schema(description = "创建时间")
     private LocalDateTime createTime;
-    @Schema(description = "更新人ID")
+    @JsonIgnore
+    @Schema(description = "更新人ID", hidden = true)
     private Long lastUpdateBy;
     @Schema(description = "更新人")
     private String lastUpdateUser;
