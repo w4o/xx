a1df2b9bf5dfa3eff7b1a998e09790f806720406
Frank <zhwuzhuo@gmail.com>
2023-03-16 08:50:45 +0800

    feat: 增加 flowable 演示接口

---
+++ xx-manage/src/main/java/com/github/w4o/xx/manage/controller/flowable/FlowableController.java
@@ -0,0 +1,72 @@
+package com.github.w4o.xx.manage.controller.flowable;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.flowable.bpmn.model.BpmnModel;
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.runtime.Execution;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.image.ProcessDiagramGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Frank
+ */
+@RestController
+@RequestMapping("/flowable")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Validated
+@Slf4j
+public class FlowableController {
+
+    private final RuntimeService runtimeService;
+    private final RepositoryService repositoryService;
+    private final ProcessEngine processEngine;
+
+    @GetMapping("/pic")
+    public void showPic(HttpServletResponse resp, String processId) throws Exception {
+        ProcessInstance pi = runtimeService.createProcessInstanceQuery().processInstanceId(processId).singleResult();
+        if (pi == null) {
+            return;
+        }
+        List<Execution> executions = runtimeService
+                .createExecutionQuery()
+                .processInstanceId(processId)
+                .list();
+
+        List<String> activityIds = new ArrayList<>();
+        List<String> flows = new ArrayList<>();
+        for (Execution exe : executions) {
+            List<String> ids = runtimeService.getActiveActivityIds(exe.getId());
+            activityIds.addAll(ids);
+        }
+
+        /*
+         * 生成流程图
+         */
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(pi.getProcessDefinitionId());
+        ProcessEngineConfiguration engconf = processEngine.getProcessEngineConfiguration();
+        ProcessDiagramGenerator diagramGenerator = engconf.getProcessDiagramGenerator();
+        byte[] buf = new byte[1024];
+        int legth;
+        try (InputStream in = diagramGenerator.generateDiagram(bpmnModel, "png", activityIds, flows, engconf.getActivityFontName(), engconf.getLabelFontName(), engconf.getAnnotationFontName(), engconf.getClassLoader(), 1.0, false); OutputStream out = resp.getOutputStream()) {
+            while ((legth = in.read(buf)) != -1) {
+                out.write(buf, 0, legth);
+            }
+        }
+    }
+
+}
