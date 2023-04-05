02025d89403d89463fcaa49b24353487fbeb8af9
Frank <zhwuzhuo@gmail.com>
2023-04-05 22:45:24 +0800

    refactor: 删除无用代码

--- xx-manage/src/main/java/com/github/w4o/xx/manage/common/config/SpringDocConfig.java
+++ xx-manage/src/main/java/com/github/w4o/xx/manage/common/config/SpringDocConfig.java
@@ -1,13 +1,9 @@
 package com.github.w4o.xx.manage.common.config;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import io.swagger.v3.core.converter.AnnotatedType;
-import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -31,14 +27,6 @@ public class SpringDocConfig {
                         .license(new License().name("MIT License").url("https://github.com/w4o/xx-service/blob/main/LICENSE")))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
-//                        .addSchemas("通用分页模型", new ObjectSchema()
-//                                .addProperties("records", new ArraySchema().title("分页数据列表"))
-//                                .addProperties("total", new IntegerSchema().title("分页总数"))
-//                                .addProperties("size", new IntegerSchema().title("每页显示条数"))
-//                                .addProperties("current", new IntegerSchema().title("当前页")))
-                        .addSchemas("aaa", ModelConverters.getInstance().resolveAsResolvedSchema(
-                                        new AnnotatedType(Page.class).resolveAsRef(false)).schema
-                                .addProperties("records", new ArraySchema().title("分页数据列表")))
                         .addSecuritySchemes(SECURITY_SCHEME_NAME,
                                 new SecurityScheme()
                                         .name(SECURITY_SCHEME_NAME)
